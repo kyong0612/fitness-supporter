a4bf9b279db5d38034b2eed5f3dd5ed81529d87b
kimkiyong <fenderst0612@gmail.com>
2024-01-02 21:12:51 +0900

    fix: lint

--- infra/config/env.go
+++ infra/config/env.go
@@ -6,8 +6,8 @@ import (
 )
 
 type config struct {
-	ENV                    string `env:"ENV"                         envDefault:"local"`
-	Port                   int    `env:"PORT"                        envDefault:"8080"`
+	ENV                    string `env:"ENV"                                envDefault:"local"`
+	Port                   int    `env:"PORT"                               envDefault:"8080"`
 	LINEChannelSecret      string `env:"LINE_CHANNEL_SECRET,required"`
 	LINEChannelAccessToken string `env:"LINE_CHANNEL_ACCESS_TOKEN,required"`
 }
