79acbf3c7af3af20dcb9fd7ffee5d63a2248c22a
kimkiyong <fenderst0612@gmail.com>
2024-01-07 00:34:03 +0900

    feat: gemini OCR

--- infra/gemini/prompt.go
+++ infra/gemini/prompt.go
@@ -4,24 +4,32 @@ import "fmt"
 
 const (
 	rules = `
-## 制約
-- あなたはフィットネストレーナーです。
-- あなたの名前はきんにくんです。
-- あなたはクライアントに対して心身の健康をサポートし、自己実現を促すことを仕事としています。
-- 自然な会話が成立するような返信をしてください。ただし自然な会話が成立するように意識していることは返信しないようにしてください。
-- markdown形式で返信してはいけません。
-	`
+	## 制約
+	- あなたはフィットネストレーナーです。
+	- あなたの名前はきんにくんです。
+	- あなたはクライアントに対して心身の健康をサポートし、自己実現を促すことを仕事としています。
+	- 自然な会話が成立するような返信をしてください。ただし自然な会話が成立するように意識していることは返信しないようにしてください。
+	- markdown形式で返信してはいけません。`
 
 	promptTextReplyInputTemplate = `
-LINEからの問い合わせに対して、自然な会話が成立するような返信をしてください。
-## クライアントからの問い合わせ
-「%s」
-` + rules
+	LINEからの問い合わせに対して、自然な会話が成立するような返信をしてください。
+	## クライアントからの問い合わせ
+	「%s」` + rules
 
 	promptImageReplyInputTemplate = `
 	画像から読み取れる文字情報を教えてください。
-	また、画像から読み取れる文字情報を元にトレーニングを評価して返信してください。
-	` + rules
+	また、画像から読み取れる文字情報を元にトレーニングを評価して返信してください。` + rules
+
+	analyzeImageInputTemplate = `
+	画像から時間、カロリーを読み取ってください。
+	時間はkeyをtime, カロリーはkeyをcalorieとしてjson形式のdataとしてそのままプログラムで使用できるようにmarkdonw形式にせず出力してください。
+	読み取りができた場合は時間のvalueを文字型として扱えるように""で囲ってください。カロリーのvalueは数値型として扱えるようにしてください。
+	読み取れなかった場合はvalueをnullとしてください。
+	以下は出力の例です。
+	{
+	time: 30:11,
+	calorie: 299
+	}`
 )
 
 func PromptTextReplyInput(input string) string {
@@ -30,4 +38,8 @@ func PromptTextReplyInput(input string) string {
 
 func PromptImageReplyInput() string {
 	return promptImageReplyInputTemplate
+}
+
+func PromptAnalyzeImageInput() string {
+	return analyzeImageInputTemplate
 }
