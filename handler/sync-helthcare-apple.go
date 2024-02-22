828642d3a700235443e00330f2be8d4f1bf24a54
kimkiyong <fenderst0612@gmail.com>
2024-02-22 22:41:56 +0900

    feat: logging body

--- handler/sync-helthcare-apple.go
+++ handler/sync-helthcare-apple.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"io"
 	"log/slog"
 	"net/http"
 	"net/http/httputil"
@@ -12,7 +13,7 @@ func (h handler) SyncHealthcareApple(w http.ResponseWriter, r *http.Request) {
 	ctx, span := otel.Tracer("").Start(r.Context(), "SyncHealthcareApple")
 	defer span.End()
 
-	dump, err := httputil.DumpRequest(r, true)
+	dump, err := httputil.DumpRequest(r, false)
 	if err != nil {
 		span.RecordError(err)
 		slog.ErrorContext(ctx, "failed to dump request", slog.Any("err", err))
@@ -21,6 +22,19 @@ func (h handler) SyncHealthcareApple(w http.ResponseWriter, r *http.Request) {
 	}
 
 	slog.InfoContext(ctx, "request dump", slog.String("dump", string(dump)))
+
+	var body []byte
+	if r.Body != nil {
+		body, err = io.ReadAll(r.Body)
+		if err != nil {
+			span.RecordError(err)
+			slog.ErrorContext(ctx, "failed to read request body", slog.Any("err", err))
+
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+
+	slog.InfoContext(ctx, "request body", slog.String("body", string(body)))
 
 	w.WriteHeader(http.StatusOK)
 }
