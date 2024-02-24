e5bedf88602682e9a320b374948c789673226564
kimkiyong <fenderst0612@gmail.com>
2024-02-24 23:21:17 +0900

    feat: publish RMU task

--- handler/sync-helthcare-apple.go
+++ handler/sync-helthcare-apple.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log/slog"
@@ -11,6 +12,8 @@ import (
 
 	"github.com/kyong0612/fitness-supporter/infra/config"
 	"github.com/kyong0612/fitness-supporter/infra/gcs"
+	"github.com/kyong0612/fitness-supporter/infra/pubsub"
+	handlerv1 "github.com/kyong0612/fitness-supporter/proto/generated/proto/handler/v1"
 	"go.opentelemetry.io/otel"
 )
 
@@ -40,10 +43,10 @@ func (h handler) SyncHealthcareApple(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// format to new-line delimited JSON
-	json := strings.ReplaceAll(string(body), "\n", "")
-	json = strings.ReplaceAll(json, "\t", "")
-	json = strings.ReplaceAll(json, " ", "")
-	body = []byte(json)
+	jsonBody := strings.ReplaceAll(string(body), "\n", "")
+	jsonBody = strings.ReplaceAll(jsonBody, "\t", "")
+	jsonBody = strings.ReplaceAll(jsonBody, " ", "")
+	body = []byte(jsonBody)
 
 	// upload sync data to gcs
 	gcsClient, err := gcs.NewClient(ctx)
@@ -60,6 +63,44 @@ func (h handler) SyncHealthcareApple(w http.ResponseWriter, r *http.Request) {
 	if err := gcsClient.Upload(ctx, bucket, fileName, body); err != nil {
 		span.RecordError(err)
 		slog.ErrorContext(ctx, "failed to upload to gcs", slog.Any("err", err))
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	// publish RMU topic
+	pubsubClient, err := pubsub.NewClient(ctx)
+	if err != nil {
+		span.RecordError(err)
+		slog.ErrorContext(ctx, "failed to create pubsub client", slog.Any("err", err))
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	data, err := json.Marshal(handlerv1.RMUAppleHealthcareRequest{
+		ObjectPath: fileName,
+	})
+	if err != nil {
+		span.RecordError(err)
+		slog.ErrorContext(ctx, "failed to marshal RMU apple healthcare request", slog.Any("err", err))
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	topicMsg, err := pubsub.NewTopic(data, "")
+	if err != nil {
+		span.RecordError(err)
+		slog.ErrorContext(ctx, "failed to create topic message", slog.Any("err", err))
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	if err := pubsubClient.PublishTopic(
+		ctx,
+		config.Get().PubSubTopicRMUHealthcareApple,
+		topicMsg,
+	); err != nil {
+		span.RecordError(err)
+		slog.ErrorContext(ctx, "failed to publish topic", slog.Any("err", err))
 
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
